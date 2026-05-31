3bc843fd8f781b6820026c76b4d22bc259517669
Keshav Allawadi <66181614+Keshav7891@users.noreply.github.com>
2026-05-31 11:44:55 +0530

    Create 283-MoveZeroes

---
+++ Data Structures & Algorithms/283-MoveZeroes/283-MoveZeroes.cpp
@@ -0,0 +1,13 @@
+// Last updated: 31/05/2026, 11:44:54
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int zeroIndex = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] != 0){
+                swap(nums[i], nums[zeroIndex]);
+                zeroIndex++;
+            }
+        }
+    }
+};
