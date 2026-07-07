d2b86fd8e1e52a4ef78637309986f080efabe1f8
Keshav Allawadi <66181614+Keshav7891@users.noreply.github.com>
2026-07-07 09:10:18 +0530

    Create 42-TrappingRainWater

---
+++ Data Structures & Algorithms/42-TrappingRainWater/42-TrappingRainWater.java
@@ -0,0 +1,26 @@
+// Last updated: 07/07/2026, 09:10:18
+class Solution {
+    public int trap(int[] height) {
+      int res = 0;
+
+      int[] lmax = new int[height.length];
+      int[] rmax = new int[height.length];
+
+      lmax[0] = height[0];
+      rmax[height.length-1] = height[height.length-1];
+
+      for(int i=1;i<height.length;i++){
+        lmax[i] = Math.max(lmax[i-1], height[i]);
+      }
+
+      for(int i=height.length-2;i>=0;i--){
+        rmax[i] = Math.max(rmax[i+1], height[i]);
+      }
+
+      for(int i=1;i<height.length-1;i++){
+        res = res + Math.min(lmax[i], rmax[i]) - height[i];
+      }
+
+      return res;
+    }
+}
