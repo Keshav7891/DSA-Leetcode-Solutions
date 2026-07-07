9265ef24fef93f4c8e8516c429988f85c3df7fbb
Keshav Allawadi <66181614+Keshav7891@users.noreply.github.com>
2026-07-07 09:09:10 +0530

    Create 961-LongPressedName

---
+++ Data Structures & Algorithms/961-LongPressedName/961-LongPressedName.java
@@ -0,0 +1,32 @@
+// Last updated: 07/07/2026, 09:09:10
+class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+        int i = 0;
+        int j = 0;
+        while(i!=name.length() && j!=typed.length()){
+            if(name.charAt(i) == typed.charAt(j)){
+                i++;
+                j++;
+            }else{
+                if(i != 0 && name.charAt(i-1) == typed.charAt(j)){
+                    j++;
+                }else{
+                    return false;
+                }
+            }
+        }
+
+        if (i != name.length()) {
+            return false;
+        }
+
+        while (j < typed.length()) {
+            if (typed.charAt(j) != typed.charAt(j - 1)) {
+                return false;
+            }
+            j++;
+        }
+
+        return true;
+    }
+}
