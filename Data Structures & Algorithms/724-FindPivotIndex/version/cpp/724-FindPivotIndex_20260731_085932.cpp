5edd77115a5113e263603a5ff3d3f53243e4573b
Keshav Allawadi <66181614+Keshav7891@users.noreply.github.com>
2026-07-31 08:59:33 +0530

    Create 724-FindPivotIndex

---
+++ Data Structures & Algorithms/724-FindPivotIndex/version/cpp/724-FindPivotIndex_20260731_085932.cpp
@@ -0,0 +1,28 @@
+// Last updated: 31/07/2026, 08:59:32
+1class Solution {
+2public:
+3    int pivotIndex(vector<int>& nums) {
+4        /*
+5        1 7 3  6  5  6
+6        1 8 11 17 22 28
+7        */
+8
+9        //build prefix sum and total sum
+10        int totalSum = 0;
+11        for(int i=0;i<nums.size();i++){
+12            totalSum = totalSum + nums[i];
+13        }
+14
+15        //check where left sum = right sum (totalsum - prefix sum till that)
+16        int leftSum = 0;
+17        for(int i=0;i<nums.size();i++){
+18            int rightSum = totalSum - leftSum - nums[i];
+19            if(leftSum == rightSum){
+20                return i;
+21            }
+22            leftSum = leftSum + nums[i];
+23        }
+24
+25        return -1;
+26    }
+27};
